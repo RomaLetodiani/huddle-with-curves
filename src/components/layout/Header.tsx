35d0ebe1592a8b69b6207e7d54a14f948910be1f
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-28 21:01:14 +0400

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,23 @@
+import SectionWrapper from '../SectionWrapper';
+import logo from '../../assets/images/logo.svg';
+import Button from '../UI/Button';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <SectionWrapper>
+      <div className="py-12 flex gap-3 items-center justify-between">
+        <img
+          className="max-[500px]:w-2/5 min-w-[150px]"
+          src={logo}
+          alt="logo"
+        />
+        <Button
+          className="bg-transparent text-pink border-pink border-2 px-8 py-2 max-[500px]:py-1 max-[500px]:px-3"
+          text="Try It Free"
+        />
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Header;
