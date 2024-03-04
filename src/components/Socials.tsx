0e7d2e17516fd9469c3d1edfcc4bffc38b7f64d6
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-04 06:15:51 +0400

    built socials component

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,21 @@
+import { socials } from './shared/consts';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex gap-3">
+      {socials.map((item, index) => (
+        <a key={index} href={item.link}>
+          <div className="cursor-pointer border border-white rounded-full p-3">
+            <img
+              className="invert brightness-0 w-6 h-6"
+              src={item.icon}
+              alt={item.alt}
+            />
+          </div>
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default Socials;
