fc7dc7aa179db46755c19e8526cd568e7823fcc5
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-04 06:14:33 +0400

    updated useInput

--- src/components/shared/hooks/UseInput.ts
+++ src/components/shared/hooks/UseInput.ts
@@ -1,28 +1,59 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export const useInput = (validate: (value: string) => boolean) => {
-  const [value, setValue] = useState('');
-  const [touched, setTouched] = useState(false);
+/**
+ * Represents the shape of the state object returned by the useInput hook.
+ */
+type InputState = {
+  /** The current value of the input field. */
+  value: string;
+  /** Indicates whether the input field is currently focused. */
+  focus: boolean;
+  /** Event handler for input changes. */
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  /** Event handler for input blur. */
+  onBlur: () => void;
+  /** Event handler for input focus. */
+  onFocus: () => void;
+  /** Indicates whether there is an error in the input value. */
+  hasError: boolean;
+};
 
-  const isValid = validate(value);
-  const hasError = !isValid && touched;
-  const reset = () => {
-    setValue('');
-    setTouched(false);
-  };
+/**
+ * A custom React hook for managing the state of an input field.
+ * @param validate A function to validate the input value.
+ * @returns An object containing input state and event handlers.
+ */
+export const useInput = (validate: (value: string) => boolean): InputState => {
+  const [value, setValue] = useState<string>('');
+  const [touched, setTouched] = useState<boolean>(false);
+  const [focus, setFocus] = useState<boolean>(false);
+
+  const isValid: boolean = validate(value);
+  const hasError: boolean = !isValid && touched;
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  const onBlur = () => {
+  const onBlur = (): void => {
     setTouched(true);
+    !value && setFocus(false);
   };
+
+  const onFocus = (): void => {
+    setFocus(true);
+  };
+
+  useEffect(() => {
+    value && setFocus(true);
+  }, [value]);
 
   return {
     value,
+    focus,
     onChange,
     onBlur,
+    onFocus,
     hasError,
-    reset,
   };
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/YOUR_REPOSITORY_NAME',
+  base: '/huddle-with-curves',
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
