52a4a2f910773b0e3a8f9aab59c228d07cf2f2cf
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-04 06:14:16 +0400

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: 'hsl(322, 100%, 66%)',
+        'light-pink': 'hsl(321, 100%, 78%)',
+        'light-red': 'hsl(0, 100%, 63%)',
+        'very-Dark-Cyan': 'hsl(192, 100%, 9%)',
+        'very-pale-blue': 'hsl(207, 100%, 98%)',
+      },
+      fontFamily: {
+        sans: 'Open Sans',
+        Poppins: 'Poppins',
+      },
+    },
   },
   plugins: [],
 };
